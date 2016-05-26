dbc6234167213262351713d7ea26edf00c6f282f
Brahm Capoor <brahm@stanford.edu>
2016-05-26 16:33:30 -0700

    Facilitated heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-python main.py
+worker: python main.py
---
+++ twitter_confidential.py
@@ -0,0 +1,14 @@
+def get_consumer_info():
+    consumer_info = {
+        'consumer_key': "hxPJmpi2a2OrpOyOyyfYZxEfV",
+        'consumer_secret': "WXnuoWKxdbpKpXl3t3sjSUUXZpzLWr7QbQ9QEgENShLvMZjAn8"
+    }
+    return consumer_info
+
+
+def get_access_info():
+    access_info = {
+        'key': "734860294179033088-9JcS4eiVsfQ48pSX2k5NEEJNp945j94",
+        'secret': "hp6nmAKJk9wgZLYT7jBqGbM2v3vZmdMo1F2VuACO5NEZV"
+    }
+    return access_info
