a87bbc9f6a74004b9462ea288e52340c337a46a0
Brahm Capoor <brahm@stanford.edu>
2016-05-26 12:44:30 -0700

    Changed sleeping time

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main():
                 emotion = f.readline()
             text = generate_tweet_text(emotion)
             tweets.post_tweet(text)
-        time.sleep(300)
+        time.sleep(1800)
 
 
 if __name__ == "__main__":
